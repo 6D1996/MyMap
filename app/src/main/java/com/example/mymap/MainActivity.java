9ca7a8409a4988d36db0a4ba9ec30bd2b95b3885
6D1996 <865570543a@gmail.com>
2021-04-20 17:05:02 +0800

    调用请求车况接口获取车辆GPS数据

--- app/src/main/java/com/example/mymap/MainActivity.java
+++ app/src/main/java/com/example/mymap/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdate;
 import com.amap.api.maps.CameraUpdateFactory;
@@ -16,9 +19,19 @@ import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.CustomMapStyleOptions;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MarkerOptions;
+import com.example.mymap.api.CarInfoReceive;
+import com.example.mymap.api.CarInfoRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Main activity
@@ -39,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     // 西南坐标
-    private LatLng southwestLatLng = new LatLng(43.821254, 125.282783);
+    private LatLng southwestLatLng = new LatLng(43.833394, 125.164146);
     // 东北坐标
     private LatLng northeastLatLng = new LatLng(43.854280, 125.300580);
 
@@ -48,6 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);// 此方法必须重写
@@ -59,9 +74,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //地图初始位置设置
         aMap.moveCamera(CameraUpdateFactory.changeLatLng(southwestLatLng));
-        aMap.moveCamera(CameraUpdateFactory.zoomTo(14f));
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(15.5f));
+
+        addressRequest();
 
     }
+
+    /**
+     * 请求车辆位置
+     */
+    public String addressRequest(){
+        CarInfoRequest carInfoRequest = new CarInfoRequest();
+        //序列化
+        final String addressRequestJson = JSON.toJSONString(carInfoRequest);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Log.d(TAG, "postAddressRequest: "+addressRequestJson);
+                    OkHttpClient addressClient=new OkHttpClient();
+                    String hostURL="http://vehicleroadcloud.faw.cn:60443/backend/appBackend/";
+                    Request addressRequest= new Request.Builder()
+                            .url(hostURL+"vehicleCondition")
+                            .post(RequestBody.create(MediaType.parse("application/json"),addressRequestJson))
+                            .build();//创造HTTP请求
+                    //执行发送的指令
+                    Log.d(TAG, "run: 请求json："+addressRequestJson);
+                    Response addressResponse = addressClient.newCall(addressRequest).execute();
+                    String carInfoString=addressResponse.body().string();
+                    Log.d(TAG, "run: 接受到了carInfoString"+carInfoString);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Log.d("POST失敗", "onClick: "+e.toString());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this,"請求地址失败！",Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+        return "";
+    }
+
+
     /**
      * 初始化AMap对象
      */
---
+++ app/src/main/java/com/example/mymap/api/DataResult.java
@@ -0,0 +1,5 @@
+package com.example.mymap.api;
+
+public class DataResult {
+
+}
